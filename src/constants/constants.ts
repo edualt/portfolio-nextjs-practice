a7a788abc04d7bde3db44bdd6316627b4fb25643
edualt <eduardosebastianhdz503@gmail.com>
2023-02-19 16:25:15 -0600

    feat(constants): modify data

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,46 +2,46 @@ export const projects = [
     {
         "title": "TODO App",
         "description": "A simple TODO app built with Django",
-        "image": "/images/1.png",
+        "image": "/images/1.jpg",
         "tags": ["Django", "Python"],
-        "source": "https://github.com/edualt/todo-django",
-        "visit": "...",
+        "source": "",
+        "visit": "https://github.com/edualt/todo-django",
         "id": 0
     },
     {
         "title": "Games Place",
         "description": "School Project. WebSite using an API developed with NodeJs and Express. Frontend built with html, css and javascript",
-        "image" : "/images/2.png",
+        "image" : "/images/2.jpg",
         "tags": ["Node", "Express", "HTML", "CSS", "JavaScript"],
-        "source": "https://github.com/edualt/games-place",
-        "visit": "...",
+        "source": "",
+        "visit": "https://github.com/edualt/games-place",
         "id": 1
     },
     {
         "title": "October Hackathon",
-        "description": "API built with Django Rest Framework that outputs a list of developer advocates with their details such as where they work, social links, bio, etc",
-        "image": "/images/3.png",
+        "description": "API built with Django Rest Framework that handle developers and enterprises info",
+        "image": "/images/3.jpg",
         "tags": ["Django Rest Framework", "Python"],
-        "source": "https://github.com/edualt/october-hackathon",
-        "visit": "...",
+        "source": "",
+        "visit": "https://github.com/edualt/october-hackathon",
         "id": 2
     },
     {
         "title": "Ecommerce-API",
         "description": "Spring boot on team, school project worked with java and spring boot framework",
-        "image": "/images/4.png",
+        "image": "/images/4.jpg",
         "tags": ["Java", "Spring Boot"],
-        "source": "https://github.com/edualt/ecommerce-upch",
-        "visit": "...",
+        "source": "",
+        "visit": "https://github.com/edualt/ecommerce-upch",
         "id": 3
     },
     {
         "title": "Ecommerce-frontend",
         "description": "School project built with ReactJs and Redux",
-        "image": "/images/5.png",
+        "image": "/images/5.jpg",
         "tags": ["React", "Redux"],
         "source": "https://github.com/edualt/ecommerce-front-upch",
-        "visit": "...",
+        "visit": "https://github.com/edualt/ecommerce-front-upch",
         "id": 4
     }
 ]
@@ -49,4 +49,5 @@ export const projects = [
 export const TimeLineData = [
     { year: 2021, text: 'Started my journey', },
     { year: 2022, text: 'Worked as backend developer in a real project', },
+    { year: 2023, text: 'Start my profesional preparation', },
 ]
