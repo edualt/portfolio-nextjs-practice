b31401e76f9d462bba8680a5db4ed5b9e19c0034
edualt <eduardosebastianhdz503@gmail.com>
2023-02-19 16:15:43 -0600

    feat(Footer): finsh component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,9 +6,23 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 export const Footer = () =>{
     return (
-        <div>
-            Footer
-        </div>
+        <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle>Call</LinkTitle>
+                    <LinkItem href="tel:961-315-2313">961-315-2313</LinkItem>
+                </LinkColumn>
+                <LinkColumn>
+                    <LinkTitle>Email</LinkTitle>
+                    <LinkItem href="mailto:edualt03@gmail.com">edualt03@gmail.com</LinkItem>
+                </LinkColumn>
+            </LinkList>
+            <SocialIconsContainer>
+                <CompanyContainer>
+                    <Slogan>Innovating one project at the time.</Slogan>
+                </CompanyContainer>
+            </SocialIconsContainer>
+        </FooterWrapper>
     )
 }
 
