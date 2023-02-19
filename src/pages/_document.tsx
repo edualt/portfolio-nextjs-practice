72915baf48082f4fd0504c8b8ba9d37f3de9d089
edualt <eduardosebastianhdz503@gmail.com>
2023-02-19 16:27:17 -0600

    fix(_document): remove innecessary data type

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitia
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
-  static async getInitialProps( ctx: DocumentContext ): Promise<DocumentInitialProps> {
+  static async getInitialProps( ctx: DocumentContext ){
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
