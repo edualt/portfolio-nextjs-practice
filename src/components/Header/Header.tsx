91d93e753e7e683e7b57119fb7a1ce001b137c0a
edualt <eduardosebastianhdz503@gmail.com>
2023-02-19 16:16:09 -0600

    feat(Header): finsh component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,47 @@
 import Link from "next/link";
-import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
+
+
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container >
+    <Div1>
+      <Link href="/" style={{display: "flex", alignItems: "center", color: "white", marginBottom: "20px"}}>
+        <DiCssdeck size="3rem"/> <Span>Portfolio</Span>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects" legacyBehavior>
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech" legacyBehavior>
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about" legacyBehavior>
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="https://github.com/edualt">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/eduardo-sebastian-hernandez-altuzar-a912bb24b/">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="https://instagram.com/_edualt">
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
